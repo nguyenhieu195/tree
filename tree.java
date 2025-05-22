b0eeb2f575d6517412d3c7fae81076594fee5532
nguyenhieu195 <nguyenhieu1952005@gmail.com>
2025-05-22 20:03:52 +0700

    up

--- tree.java
+++ tree.java
@@ -113,7 +113,6 @@ public class tree {
     int SoLa() {
         return demsola(root);
     }
-
     int tongnodetrong(tNode t) { // là node không phải node lá
         if (t == null) {
             return 0;
